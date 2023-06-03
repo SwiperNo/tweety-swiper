8d30a68679d0ab06a6512e3b0933f2092932d75a
Kyle Jones <kylejones199108@gmail.com>
2023-06-03 01:09:25 -0500

    modified:   app/GW_Twitter_Bot.py

--- app/GW_Twitter_Bot.py
+++ app/GW_Twitter_Bot.py
@@ -8,19 +8,25 @@ env_path = Path('C:\\Users\\kjon5\\repos\\tweety-swiper\\') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 # Set up Twitter API authentication
-consumer_key = os.getenv('consumer_key')
-consumer_secret = os.getenv('consumer_secret')
-access_token = os.getenv('access_token')
-access_token_secret = os.getenv('access_token_secret')
+try:
+    consumer_key = os.getenv('consumer_key')
+    consumer_secret = os.getenv('consumer_secret')
+    access_token = os.getenv('access_token')
+    access_token_secret = os.getenv('access_token_secret')
 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
+except Exception as e:
+    print(f"Error setting up Twitter API: {e}")
 
 # Set up OpenAI API authentication
-openai.api_key = os.getenv('openai_api_key')
+try:
+    openai.api_key = os.getenv('openai_api_key')
+except Exception as e:
+    print(f"Error setting up OpenAI API: {e}")
 
 #Added prompts -kj
 # Define a list of prompts
@@ -31,18 +37,21 @@ prompts = [
 
 # Updated Response to include desired values and prompt -kj
 # Call the ChatGPT API using a random prompt from the list
-prompt = prompts[random.randint(0, len(prompts)-1)]
-response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=prompt,
-    max_tokens=20,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+try: 
+    prompt = prompts[random.randint(0, len(prompts)-1)]
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=20,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
 
-# Extract the response from the API output
-tweet_text = response.choices[0].text.strip()
+    # Extract the response from the API output
+    tweet_text = response.choices[0].text.strip()
+except Exception as e:
+    print(f"Error generating tweet text: {e}")
 
 # Added the user_approval var -kj
 # Wait for user approval
@@ -52,8 +61,11 @@ while user_approval.upper() not in ["YES", "NO"]:
 
 # Post the tweet and image if approved
 if user_approval.upper() == "YES":
-    # Update the Twitter status with the image
-    api.update_status(tweet_text)
-    print("Tweet posted successfully!")
+    try:
+        # Update the Twitter status with the image
+        api.update_status(tweet_text)
+        print("Tweet posted successfully!")
+    except Exception as e:
+        print(f"Error posting tweet: {e}")
 else:
     print("Tweet not posted.")
